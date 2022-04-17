5ab1e1fbb3c316b74ff965ba435b8276fb2dc4a4
iddel_ssdc <logan.drum@iddel.ca>
2022-04-17 10:28:04 -0230

    Added a weeks worth of playlists (1-7) and a default playlist (0). Updated file-loader to load in the populated files

--- src/app/services/file-loader.service.ts
+++ src/app/services/file-loader.service.ts
@@ -1,10 +1,12 @@
 import { Injectable } from '@angular/core';
 
-import * as playlist0 from 'src/assets/playlists/playlist0.json'
-import * as playlist1 from 'src/assets/playlists/playlist1.json'
-import * as playlist2 from 'src/assets/playlists/playlist2.json'
-import * as schedule1 from 'src/assets/schedules/schedule1.json'
-import * as schedule2 from 'src/assets/schedules/schedule2.json'
+import * as playlist0 from 'src/assets/playlists/playlist0.json';
+import * as playlist1 from 'src/assets/playlists/playlist1.json';
+import * as playlist2 from 'src/assets/playlists/playlist2.json';
+import * as playlist3 from 'src/assets/playlists/playlist3.json';
+import * as playlist4 from 'src/assets/playlists/playlist4.json';
+import * as schedule1 from 'src/assets/schedules/schedule1.json';
+import * as schedule2 from 'src/assets/schedules/schedule2.json';
 
 @Injectable({
   providedIn: 'root'
@@ -27,6 +29,10 @@ export class FileLoaderService {
       return playlist1;
     } else if(playlist_selector == 2) {
       return playlist2;
+    } else if(playlist_selector == 3) {
+      return playlist3;
+    } else if(playlist_selector == 4) {
+      return playlist4;
     } else {
       return playlist0;
     }
